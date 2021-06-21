1952fc2048a46644f2606e2a4ce1276a4580eaaf
eivind <eivinddyrlie1@gmail.com>
2021-06-21 18:04:06 +0200

    startup

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+        echo 'index'; 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>blogger.disk</title>
+</head>
+<body>
+<header>
+        <h1>Blogger.disk</h1>
+</header>
+<nav>
+        <div></div>
+</nav>
+<div>
+        
+</div>
+<footer>
+        
+</footer>
+</body>
+</html>
